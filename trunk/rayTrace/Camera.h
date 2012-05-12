4d6c43a625566b6fc1edd4bd885e4009f4b298c7
walthermaciel@gmail.com <walthermaciel@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-05-12 13:27:55 +0000

    adicionando cabeçalhos dos métodos

--- trunk/rayTrace/Camera.h
+++ trunk/rayTrace/Camera.h
@@ -8,11 +8,54 @@
 #ifndef CAMERA_H_
 #define CAMERA_H_
 
+#include "Vector3D.h"
+
 class Camera
 {
 public:
+    /**
+     * Constructor
+     */
     Camera();
+
+    /**
+     * Desctructor
+     */
     virtual ~Camera();
+
+private:
+    /** Position of the camera on world space */
+    Vector3D _eye;
+
+    /** Up vector of the camera */
+    Vector3D _up;
+
+    /** Point in the world the camera is looking at */
+    Vector3D _center;
+
+    /** Field of vision */
+    double _fovy;
+
+    /** distance to near plane */
+    double _near;
+
+    /** distance to far plane */
+    double _far;
+
+    /** width of the screen in pixels */
+    int _width;
+
+    /** height of the screen in pixels */
+    int _height;
+
+    /** width of screen in world units */
+    double a;
+
+    /** height of screen in world units */
+    double b;
+
+    /** base of the eye space */
+    Vector3D _eyeBase;
 };
 
 #endif /* CAMERA_H_ */
