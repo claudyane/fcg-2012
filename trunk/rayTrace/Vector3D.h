66936a3fbcafebcb8bcaa9286cac87b3f8e25daf
walthermaciel@gmail.com <walthermaciel@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-05-12 13:04:46 +0000

    Adicionando classe dummy do vector3d

---
+++ trunk/rayTrace/Vector3D.h
@@ -0,0 +1,18 @@
+/*
+ * Vector3D.h
+ *
+ *  Created on: May 12, 2012
+ *      Author: walther
+ */
+
+#ifndef VECTOR3D_H_
+#define VECTOR3D_H_
+
+class Vector3D
+{
+public:
+    Vector3D();
+    virtual ~Vector3D();
+};
+
+#endif /* VECTOR3D_H_ */
