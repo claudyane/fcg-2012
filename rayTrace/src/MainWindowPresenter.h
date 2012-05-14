4ea4740dea3b3fcdf6d83942375542005a157b3e
walthermaciel@gmail.com <walthermaciel@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-05-14 21:15:34 +0000

    adicionand a classe presenter

---
+++ rayTrace/src/MainWindowPresenter.h
@@ -0,0 +1,18 @@
+/*
+ * MainWindowPresenter.h
+ *
+ *  Created on: May 14, 2012
+ *      Author: walther
+ */
+
+#ifndef MAINWINDOWPRESENTER_H_
+#define MAINWINDOWPRESENTER_H_
+
+class MainWindowPresenter
+{
+public:
+    MainWindowPresenter();
+    virtual ~MainWindowPresenter();
+};
+
+#endif /* MAINWINDOWPRESENTER_H_ */
