fa81dcf28d0e4fa1dd49523035ca264b0502aad2
walthermaciel@gmail.com <walthermaciel@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-05-14 02:34:08 +0000

    classe dummy da janela

---
+++ rayTrace/src/MainWindow.cpp
@@ -0,0 +1,20 @@
+/*
+ * MainWindow.cpp
+ *
+ *  Created on: May 13, 2012
+ *      Author: walther
+ */
+
+#include "MainWindow.h"
+
+MainWindow::MainWindow()
+{
+    // TODO Auto-generated constructor stub
+    
+}
+
+MainWindow::~MainWindow()
+{
+    // TODO Auto-generated destructor stub
+}
+
