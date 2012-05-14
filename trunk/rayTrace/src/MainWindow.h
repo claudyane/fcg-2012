d664d9a972e293f383fdc9a39abf5f510e7cb32a
walthermaciel@gmail.com <walthermaciel@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-05-14 02:34:08 +0000

    classe dummy da janela

---
+++ trunk/rayTrace/src/MainWindow.h
@@ -0,0 +1,21 @@
+/*
+ * MainWindow.h
+ *
+ *  Created on: May 13, 2012
+ *      Author: walther
+ */
+
+#ifndef MAINWINDOW_H_
+#define MAINWINDOW_H_
+
+class MainWindow
+{
+public:
+    MainWindow();
+    virtual ~MainWindow();
+
+private:
+
+};
+
+#endif /* MAINWINDOW_H_ */
