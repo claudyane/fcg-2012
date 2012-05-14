35e46ba926b0dba26cfc86916b11f973b668c4c8
walthermaciel@gmail.com <walthermaciel@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-05-14 21:15:34 +0000

    adicionand a classe presenter

---
+++ trunk/rayTrace/src/MainWindowPresenter.cpp
@@ -0,0 +1,20 @@
+/*
+ * MainWindowPresenter.cpp
+ *
+ *  Created on: May 14, 2012
+ *      Author: walther
+ */
+
+#include "MainWindowPresenter.h"
+
+MainWindowPresenter::MainWindowPresenter()
+{
+    // TODO Auto-generated constructor stub
+    
+}
+
+MainWindowPresenter::~MainWindowPresenter()
+{
+    // TODO Auto-generated destructor stub
+}
+
