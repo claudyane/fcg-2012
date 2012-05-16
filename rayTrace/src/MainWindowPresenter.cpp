a3ae8998c6062df5e289e5085def87c5fe8c8bb3
lili.goldner@gmail.com <lili.goldner@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-05-16 20:31:04 +0000

    implementa callback de fechar e abrir arquivo

--- rayTrace/src/MainWindowPresenter.cpp
+++ rayTrace/src/MainWindowPresenter.cpp
@@ -9,21 +9,35 @@
 #include "MainWindow.h"
 #include "Scene.h"
 
+
 MainWindowPresenter::MainWindowPresenter()
 {
     _image = NULL;
     _scene  = NULL;
 }
 
+
+
 MainWindowPresenter::~MainWindowPresenter()
 {
     // TODO Auto-generated destructor stub
 }
 
+
+
 Image* MainWindowPresenter::getImage()
 {
     return _image;
 }
+
+
+
+bool MainWindowPresenter::buildScene( std::string filename )
+{
+    return false;
+}
+
+
 
 void MainWindowPresenter::renderScene()
 {
