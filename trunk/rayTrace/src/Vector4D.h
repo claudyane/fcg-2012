0bc8b81212b1aad12961fb0cbc2e74bd02758cd9
walthermaciel@gmail.com <walthermaciel@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-05-16 19:29:19 +0000

    Adiciona método de set ao vector4d

--- trunk/rayTrace/src/Vector4D.h
+++ trunk/rayTrace/src/Vector4D.h
@@ -10,68 +10,74 @@
 
 class Vector4D
 {
-	public:
+public:
 
-		/**
-		 * Standard constructor
-		 * Initializes the (0, 0, 0, 1) vector
-		 */
-		Vector4D();
+    /**
+     * Standard constructor
+     * Initializes the (0, 0, 0, 1) vector
+     */
+    Vector4D();
 
-		/**
-		 * Copy constructor
-		 */
-		Vector4D( const Vector4D& copy );
+    /**
+     * Copy constructor
+     */
+    Vector4D( const Vector4D& copy );
 
-		/**
-		 * Constructor
-		 * @param[in] x Vector's x coordinate
-		 * @param[in] y Vector's y coordinate
-		 * @param[in] z Vector's z coordinate
-		 */
-		Vector4D( double x, double y, double z, double w );
+    /**
+     * Constructor
+     * @param[in] x Vector's x coordinate
+     * @param[in] y Vector's y coordinate
+     * @param[in] z Vector's z coordinate
+     */
+    Vector4D( double x, double y, double z, double w );
 
-		/**
-		 * Destructor
-		 */
-		virtual ~Vector4D();
+    /**
+     * Destructor
+     */
+    virtual ~Vector4D();
 
-        /**
-         * Returns squared norm
-         */
-        double norm2();
+    /**
+     * Sets the x, y, z and w values of the vector.
+     * @param x Values of the x coordinate
+     * @param y Values of the y coordinate
+     * @param z Values of the z coordinate
+     * @param w Values of the w coordinate
+     */
+    void set( double x, double y, double z, double w );
+    
+    /**
+     * Returns squared norm
+     */
+    double norm2();
 
-        /**
-         * Return norm
-         */
-        double norm();
+    /**
+     * Return norm
+     */
+    double norm();
 
-        /**
-         * Normalizes the vector
-         */
-        void normalize();
+    /**
+     * Normalizes the vector
+     */
+    void normalize();
 
-        /**
-         * Operators
-         */
+    /**
+     * Operators
+     */
 
-        Vector4D operator=( const Vector4D& a );
-        Vector4D operator+=( const Vector4D& a );
-        Vector4D operator-=( const Vector4D& a );
-        Vector4D operator*=( const double a );
-        Vector4D operator/=( const double a );
-        Vector4D operator-();
+    Vector4D operator=( const Vector4D& a );
+    Vector4D operator+=( const Vector4D& a );
+    Vector4D operator-=( const Vector4D& a );
+    Vector4D operator*=( const double a );
+    Vector4D operator/=( const double a );
+    Vector4D operator-();
 
-		/**
-		 * Vector's coordinates
-		 */
-		double x;
-		double y;
-		double z;
-		double w;
-
-	private:
-
+    /**
+     * Vector's coordinates
+     */
+    double x;
+    double y;
+    double z;
+    double w;   
 };
 
 /**
