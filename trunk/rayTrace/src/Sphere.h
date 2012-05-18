917baebf36b20494309493b69d71a93ac302ffc5
walthermaciel@gmail.com <walthermaciel@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-05-18 22:49:19 +0000

    Commit para debugging

--- trunk/rayTrace/src/Sphere.h
+++ trunk/rayTrace/src/Sphere.h
@@ -17,9 +17,8 @@ public:
     Sphere( double centerX, double centerY, double centerZ, double radius );
 
     virtual ~Sphere();
-    bool computeRayIntersection( Ray ray, Vector4D& point, Vector4D& normal );
+    bool computeRayIntersection( Ray ray, double& t, Vector4D& normal );
     void getColor( float& r, float& g, float& b, float& a );
-    bool computeRayIntersection( Ray ray, Vector4D& point );
     void setMaterial( Material* material );
 
 private:
