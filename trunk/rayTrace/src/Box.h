4e701180e206380152590273cd6a7899335b9aaa
lili.goldner@gmail.com <lili.goldner@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-05-19 01:16:48 +0000

    Cria objeto BOX

---
+++ trunk/rayTrace/src/Box.h
@@ -0,0 +1,42 @@
+/* 
+ * File:   Box.h
+ * Author: eliana
+ *
+ * Created on May 18, 2012, 9:17 PM
+ */
+
+#ifndef BOX_H
+#define	BOX_H
+
+#include "Material.h"
+#include "Vector4D.h"
+#include "Ray.h"
+#include "Object.h"
+
+class Box : public Object
+{
+public:
+    
+    Box( Vector4D min, Vector4D max );
+    
+    virtual ~Box();
+    
+    bool computeRayIntersection( Ray ray, double& t );
+    
+    Vector4D getNormal( Vector4D& point );
+    
+    void getColor( float& r, float& g, float& b, float& a );
+    
+    void setMaterial( Material* material );
+    
+private:
+
+    Material* _material;
+    
+    Vector4D _min;
+    
+    Vector4D _max;
+};
+
+#endif	/* BOX_H */
+
