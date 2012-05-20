3f2585661ce71c9f78e9b666d263a191c1c3c0d9
lili.goldner@gmail.com <lili.goldner@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-05-20 22:41:42 +0000

    Cria classe color

---
+++ trunk/rayTrace/src/Color.cpp
@@ -0,0 +1,98 @@
+#include "Color.h"
+
+
+Color::Color()
+{
+    
+}
+
+
+
+Color::Color( float r, float g, float b )
+{
+    
+}
+
+
+
+Color::Color( const Color& copy )
+{
+    
+}
+
+
+
+Color::~Color()
+{
+    
+}
+
+
+
+void Color::set( double r, double g, double b )
+{
+    
+}
+
+
+
+Color Color::operator=( const Color& a )
+{
+    
+}
+
+
+
+Color Color::operator+=( const Color& a )
+{
+    
+}
+
+
+
+Color Color::operator-=( const Color& a )
+{
+    
+}
+
+
+
+Color Color::operator*=( const double a )
+{
+   
+}
+
+
+
+Color Color::operator/=( const double a )
+{
+    
+}
+
+
+
+Color operator+( const Color& a, const Color& b )
+{
+    
+}
+
+
+
+Color operator-( const Color& a, const Color& b )
+{
+    
+}
+
+
+
+Color operator*( const double a, const Color& b )
+{
+    
+}
+
+
+
+Color operator*( const Color& a, const double b )
+{
+    
+}
---
+++ trunk/rayTrace/src/Color.h
@@ -0,0 +1,72 @@
+/* 
+ * File:   Color.h
+ * Author: eliana
+ *
+ * Created on May 20, 2012, 7:28 PM
+ */
+
+#ifndef COLOR_H
+#define	COLOR_H
+
+class Color 
+{
+public:
+    
+    /**
+     * Default constructor
+     * Initializes the (0, 0, 0) color
+     */
+    Color();
+    
+    /**
+     * Constructor
+     * @param r Red component
+     * @param g Green component
+     * @param b Blue component
+     */
+    Color( float r, float g, float b );
+
+    /**
+     * Copy constructor
+     */
+    Color( const Color& copy );
+    
+    /**
+     * Destructor
+     */
+    virtual ~Color();
+    
+    /**
+     * Set color components value
+     * @param r Red component
+     * @param g Green component
+     * @param b Blue component
+     */
+    void set( double r, double g, double b );
+    
+    /**
+     * Operators
+     */
+    Color operator=( const Color& a );
+    Color operator+=( const Color& a );
+    Color operator-=( const Color& a );
+    Color operator*=( const double a );
+    Color operator/=( const double a );
+    
+    /** Color components */
+    float r;
+    float g;
+    float b;
+};
+
+/**
+ * Operators
+ */
+Color operator+( const Color& a, const Color& b );
+Color operator-( const Color& a, const Color& b );
+Color operator*( const double a, const Color& b );
+Color operator*( const Color& a, const double b );
+
+
+#endif	/* COLOR_H */
+
