426dcfb72247394daa74d05a6715952882eb31a6
lili.goldner@gmail.com <lili.goldner@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-05-20 22:47:36 +0000

    Implementa classe color

--- rayTrace/src/Color.cpp
+++ rayTrace/src/Color.cpp
@@ -3,21 +3,27 @@
 
 Color::Color()
 {
-    
+    r = 0.0f;
+    g = 0.0f;
+    b = 0.0f;
 }
 
 
 
 Color::Color( float r, float g, float b )
 {
-    
+    this->r = r;
+    this->g = g;
+    this->b = b;
 }
 
 
 
 Color::Color( const Color& copy )
 {
-    
+    r = copy.r;
+    g = copy.g;
+    b = copy.b;
 }
 
 
@@ -31,68 +37,90 @@ Color::~Color()
 
 void Color::set( double r, double g, double b )
 {
-    
+    this->r = r;
+    this->g = g;
+    this->b = b;
 }
 
 
 
 Color Color::operator=( const Color& a )
 {
+    r = a.r;
+    g = a.g;
+    b = a.b;
     
+    return *this;
 }
 
 
 
 Color Color::operator+=( const Color& a )
 {
+    r += a.r;
+    g += a.g;
+    b += a.b;
     
+    return *this;
 }
 
 
 
 Color Color::operator-=( const Color& a )
 {
+    r -= a.r;
+    g -= a.g;
+    b -= a.b;
     
+    return *this;
 }
 
 
 
 Color Color::operator*=( const double a )
 {
-   
+    r *= a;
+    g *= a;
+    b *= a;
+    
+    return *this;
 }
 
 
 
 Color Color::operator/=( const double a )
 {
+    r /= a;
+    g /= a;
+    b /= a;
     
+    return *this;    
 }
 
 
 
 Color operator+( const Color& a, const Color& b )
 {
-    
+    return Color( a.r + b.r, a.g + b.g, a.b + b.b );
 }
 
 
 
 Color operator-( const Color& a, const Color& b )
 {
-    
+    return Color( a.r - b.r, a.g - b.g, a.b - b.b );
 }
 
 
 
 Color operator*( const double a, const Color& b )
 {
-    
+    return Color( a * b.r , a * b.g, a * b.b );
 }
 
 
 
 Color operator*( const Color& a, const double b )
 {
-    
+    return Color( a.r * b, a.g * b, a.b * b );
 }
