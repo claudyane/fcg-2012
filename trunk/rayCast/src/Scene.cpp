13a3ccb88a841e175d53cc5ad1452f96f2515938
walthermaciel@gmail.com <walthermaciel@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-05-28 20:02:15 +0000

    Adicionando classes Scene e volume

---
+++ trunk/rayCast/src/Scene.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Scene.cpp
+ * Author: walther
+ * 
+ * Created on May 28, 2012, 5:00 PM
+ */
+
+#include "Scene.h"
+
+Scene::Scene() {
+}
+
+Scene::Scene(const Scene& orig) {
+}
+
+Scene::~Scene() {
+}
+
