f511d58de7e92716f22d97e84672257feb264a66
walthermaciel@gmail.com <walthermaciel@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-05-29 01:27:54 +0000

    Classe color possui componente alpha

--- rayCast/src/Color.cpp
+++ rayCast/src/Color.cpp
@@ -6,15 +6,17 @@ Color::Color()
     r = 0.0f;
     g = 0.0f;
     b = 0.0f;
+    a = 1.0f;
 }
 
 
 
-Color::Color( float r, float g, float b )
+Color::Color( float r, float g, float b, float a )
 {
     this->r = r;
     this->g = g;
     this->b = b;
+    this->a = a;
 }
 
 
@@ -24,111 +26,115 @@ Color::Color( const Color& copy )
     r = copy.r;
     g = copy.g;
     b = copy.b;
+    a = copy.a;
 }
 
 
 
-Color::~Color()
-{
-    
-}
+Color::~Color(){}
 
 
 
-void Color::set( float r, float g, float b )
+void Color::set( float r, float g, float b, float a )
 {
     this->r = r;
     this->g = g;
     this->b = b;
+    this->a = a;
 }
 
 
 
-Color Color::operator=( const Color& a )
+Color Color::operator=( const Color& c )
 {
-    r = a.r;
-    g = a.g;
-    b = a.b;
+    r = c.r;
+    g = c.g;
+    b = c.b;
+    a = c.a;
     
     return *this;
 }
 
 
 
-Color Color::operator+=( const Color& a )
+Color Color::operator+=( const Color& c )
 {
-    r += a.r;
-    g += a.g;
-    b += a.b;
+    r += c.r;
+    g += c.g;
+    b += c.b;
+    a += c.a;
     
     return *this;
 }
 
 
 
-Color Color::operator-=( const Color& a )
+Color Color::operator-=( const Color& c )
 {
-    r -= a.r;
-    g -= a.g;
-    b -= a.b;
+    r -= c.r;
+    g -= c.g;
+    b -= c.b;
+    a -= c.a;
     
     return *this;
 }
 
 
 
-Color Color::operator*=( const double a )
+Color Color::operator*=( const double c )
 {
-    r *= a;
-    g *= a;
-    b *= a;
+    r *= c;
+    g *= c;
+    b *= c;
+    a *= c;
     
     return *this;
 }
 
 
 
-Color Color::operator/=( const double a )
+Color Color::operator/=( const double c )
 {
-    r /= a;
-    g /= a;
-    b /= a;
+    r /= c;
+    g /= c;
+    b /= c;
+    a /= c;
     
     return *this;    
 }
 
 
 
-Color operator+( const Color& a, const Color& b )
+Color operator+( const Color& c, const Color& b )
 {
-    return Color( a.r + b.r, a.g + b.g, a.b + b.b );
+    return Color( c.r + b.r, c.g + b.g, c.b + b.b, c.a + b.a );
 }
 
 
 
-Color operator-( const Color& a, const Color& b )
+Color operator-( const Color& c, const Color& b )
 {
-    return Color( a.r - b.r, a.g - b.g, a.b - b.b );
+    return Color( c.r - b.r, c.g - b.g, c.b - b.b, c.a - b.a );
 }
 
 
 
-Color operator*( const double a, const Color& b )
+Color operator*( const double c, const Color& b )
 {
-    return Color( a * b.r , a * b.g, a * b.b );
+    return Color( c * b.r , c * b.g, c * b.b, c * b.a );
 }
 
 
 
-Color operator*( const Color& a, const double b )
+Color operator*( const Color& c, const double b )
 {
-    return Color( a.r * b, a.g * b, a.b * b );
+    return Color( c.r * b, c.g * b, c.b * b, c.a * b );
 }
 
 
 
-Color operator*( const Color& a, const Color& b )
+Color operator*( const Color& c, const Color& b )
 {
-    return Color( a.r * b.r, a.g * b.g, a.b * b.b );
+    return Color( c.r * b.r, c.g * b.g, c.b * b.b, c.a * b.a );
 }
 
