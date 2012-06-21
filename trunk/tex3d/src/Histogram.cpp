ad6e5265e683228166488c4d468c9e5a4f38e7f9
walthermaciel@gmail.com <walthermaciel@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-06-21 19:55:28 +0000

    cria classe histograma

---
+++ trunk/tex3d/src/Histogram.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   Histogram.cpp
+ * Author: walther
+ * 
+ * Created on June 21, 2012, 4:55 PM
+ */
+
+#include "Histogram.h"
+
+Histogram::Histogram()
+{
+}
+
+Histogram::Histogram(const Histogram& orig) {
+}
+
+Histogram::~Histogram() {
+}
+
---
+++ trunk/tex3d/src/Histogram.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Histogram.h
+ * Author: walther
+ *
+ * Created on June 21, 2012, 4:55 PM
+ */
+
+#ifndef HISTOGRAM_H
+#define	HISTOGRAM_H
+
+class Histogram {
+public:
+    Histogram();
+    Histogram(const Histogram& orig);
+    virtual ~Histogram();
+private:
+
+};
+
+#endif	/* HISTOGRAM_H */
+
