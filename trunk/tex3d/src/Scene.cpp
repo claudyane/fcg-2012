6c927d1f4074646972fe99ad7b816ea34d0a8f25
lili.goldner@gmail.com <lili.goldner@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-06-21 19:52:35 +0000

    define shading model

--- trunk/tex3d/src/Scene.cpp
+++ trunk/tex3d/src/Scene.cpp
@@ -223,6 +223,7 @@ void Scene::render()
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
     glEnable( GL_DEPTH_TEST );
     glDisable( GL_CULL_FACE );
+    glShadeModel( GL_SMOOTH );
     
     // Desenha a caixa envolvente
     drawBox();
