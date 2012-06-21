756ef431a783f6673912d1ab42c04aadcd18718e
walthermaciel@gmail.com <walthermaciel@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-06-21 20:10:20 +0000

    implementação de algumas funções do histograma

--- trunk/tex3d/src/Histogram.cpp
+++ trunk/tex3d/src/Histogram.cpp
@@ -5,16 +5,45 @@
  * Created on June 21, 2012, 4:55 PM
  */
 
+#include <cstring>
+
 #include "Histogram.h"
 
 Histogram::Histogram()
 {
+    clear();
 }
 
 Histogram::~Histogram()
 {
+    // do nothing
 }
 
-void Histogram::setVolume(Volume* volume){}
+void Histogram::setVolume(Volume* volume)
+{
+    clear();
+    
+    int nx, ny, nz;
+    volume->getNumberOfSamples( nx, ny, nz );
+    
+    for( int x = 0; x < nx; ++x )
+    {
+        for( int y = 0; y < ny; ++y )
+        {
+            for( int z = 0; z < nz; ++z )
+            {
+                byte value = volume->getVoxel( x, y, z );
+                _values[value] += 1;
+            }
+        }
+    }
+}
 
-void Histogram::draw(){}
+void Histogram::draw()
+{
+}
+
+void Histogram::clear()
+{
+    memset( _values, 0, sizeof( _values ) );
+}
