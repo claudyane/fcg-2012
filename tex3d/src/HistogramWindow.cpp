ddcad547fa53094e353e03cf73d4b3102a5238e0
lili.goldner@gmail.com <lili.goldner@gmail.com@33a9351a-dda2-ef57-0b55-d12c60e3de41>
2012-06-22 03:02:46 +0000

    desenha o histograma

--- tex3d/src/HistogramWindow.cpp
+++ tex3d/src/HistogramWindow.cpp
@@ -8,7 +8,7 @@
 HistogramWindow::HistogramWindow()
 {   
     _width = 1100;
-    _height = 300;
+    _height = 550;
     
     _histogram = new Histogram( _width, _height );
     _window = build();
